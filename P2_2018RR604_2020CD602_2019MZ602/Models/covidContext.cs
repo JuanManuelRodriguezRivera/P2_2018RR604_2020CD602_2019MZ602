1ba470f37752813e68dcc7c23f1f4d11da1f6023
JuanManuelRodriguezRivera <juan.rodriguez7@catolica.edu.sv>
2023-04-29 07:45:23 -0600

    Context

---
+++ P2_2018RR604_2020CD602_2019MZ602/Models/covidContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P2_2018RR604_2020CD602_2019MZ602.Models
+{
+    public class covidDbContext: DbContext
+    {
+        public covidDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+    }
+}
