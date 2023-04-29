e388d160dc9b8f5c1ccb9f817d08a79e3eb428c8
Gustavo Cuéllar <Gustavo Cullar@TAVOJAJA>
2023-04-29 09:14:21 -0600

    Creacion de modelo Departamentos

---
+++ P2_2018RR604_2020CD602_2019MZ602/Models/Departamentos.cs
@@ -0,0 +1,14 @@
+﻿    using MessagePack;
+using System.ComponentModel.DataAnnotations;
+
+namespace P2_2018RR604_2020CD602_2019MZ602.Models
+{
+    public class Departamentos
+    {
+        [Key]
+
+        public int id_departamentos { get; set; }
+        public string nombre_departamento { get; set; }
+        
+    }
+}
--- P2_2018RR604_2020CD602_2019MZ602/Models/covidContext.cs
+++ P2_2018RR604_2020CD602_2019MZ602/Models/covidContext.cs
@@ -3,10 +3,12 @@ using P2_2018RR604_2020CD602_2019MZ602.Models;
 
 namespace P2_2018RR604_2020CD602_2019MZ602.Models
 {
-    public class covidDbContext: DbContext
+    public class covidDbContext : DbContext
     {
-        public covidDbContext(DbContextOptions<covidDbContext> options) : base(options) {}
+        public covidDbContext(DbContextOptions<covidDbContext> options) : base(options) { }
 
         public DbSet<Generos> Generos { get; set; }
+
+        public DbSet<Departamentos> Departamentos {get; set;} 
     }
 }
